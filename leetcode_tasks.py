cd1a4db500e8a8c43c6816c6b374c7ac4485a6ff
Al.Kazakov <t-dayne@mail.ru>
2022-10-30 14:33:00 +0300

    #new_task_done

--- leetcode_tasks.py
+++ leetcode_tasks.py
@@ -20,6 +20,7 @@ class Best_Time_to_Buy_and_Sell_Stock:
             return 0
         return max_bound - min_bound
 
+
 class lengthOfLastWord:
     """
     Given a string s consisting of words and spaces, return the length of the last word in the string.
@@ -28,6 +29,7 @@ class lengthOfLastWord:
     Runtime: 61 ms, faster than 27.97% of Python3 online submissions for Length of Last Word.
     Memory Usage: 14 MB, less than 5.39% of Python3 online submissions for Length of Last Word.
     """
+
     def lengthOfLastWord(self, s: str) -> int:
         from string import ascii_letters, whitespace
         good_chars = (ascii_letters + whitespace).encode()
@@ -37,3 +39,36 @@ class lengthOfLastWord:
         words = s.split(" ")
         words = [x for x in words if x != '']
         return len(words[-1])
+
+
+class isPalindrome:
+    """
+    Given an integer x, return true if x is a palindrome, and false otherwise.
+    Follow up: Could you solve it without converting the integer to a string?
+
+    Runtime: 101 ms, faster than 72.79% of Python3 online submissions for Palindrome Number.
+    Memory Usage: 13.9 MB, less than 59.46% of Python3 online submissions for Palindrome Number.
+    """
+
+    def isPalindrome(self, x: int) -> bool:
+        numbers = []
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+
+        def go_deep(i, numbers):
+            numbers.append(i % 10)
+            new_i = i // 10
+            if new_i > 0:
+                go_deep(new_i, numbers)
+            if len(numbers) < 2:
+                return True
+            if numbers[-1] == numbers[0]:
+                numbers.pop(0)
+                numbers.pop(-1)
+            return False
+
+        return go_deep(x, numbers)
+
+
+o = isPalindrome()
+print(o.isPalindrome(10000))
